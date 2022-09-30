4ef00227fe3c2632ec6cb2376053e05bc25bdb4f
Chuck Smith <chuck@eclecticsaddlebag.com>
2022-09-30 15:35:15 -0400

    Add modal for user profile editing

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -13,4 +13,27 @@ class Users::DashboardController < ApplicationController
       redirect_to users_account_show_path
     end
   end
+
+  private
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= current_user
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def resource_class
+    User
+  end
+
+  helper_method :resource_name
+  helper_method :resource
+  helper_method :devise_mapping
+  helper_method :resource_class
 end
