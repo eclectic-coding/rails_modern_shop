15fcb68b8340e95816c6812e17d8547bc546553a
Chuck Smith <chuck@eclecticsaddlebag.com>
2022-10-04 15:51:38 -0400

    Add begining seeds

---
+++ db/seeds/01 - users.rb
@@ -0,0 +1,12 @@
+User.delete_all
+
+User.create!(name: "Admin User", email: "admin@example.com", password: "Mysecret+1234",
+  password_confirmation: "Mysecret+1234", role: "admin")
+
+User.create!(name: "Customer User", email: "customer@example.com", password: "Mysecret+1234",
+  password_confirmation: "Mysecret+1234")
+
+User.create!(name: "Sample User", email: "sample@example.com", password: "Mysecret+1234",
+              password_confirmation: "Mysecret+1234")
+
+puts "Seed users complete"
