298e78c4260dc07e9df344a37929d1e300797b1c
Chuck <chuck@eclecticsaddlebag.com>
2023-04-21 09:11:42 -0400

    Add search to admin users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
 
   has_person_name
 
@@ -11,13 +11,18 @@ class User < ApplicationRecord
     customer: "customer"
   }
 
-  FILTER_PARAMS = %i[role column direction].freeze
+  FILTER_PARAMS = %i[search role column direction].freeze
 
   scope :by_role, ->(role) { where(role: role) if role.present? }
+  scope :by_terms, ->(query) {
+                     where("users.first_name ilike ? OR users.last_name ilike ? OR users.email ilike ?",
+                       "%#{query}%", "%#{query}%", "%#{query}%")
+                   }
 
   def self.filter(filters)
     User
       .by_role(filters["role"])
+      .by_terms(filters["search"])
       .order("#{filters["column"]} #{filters["direction"]}")
   end
 end
