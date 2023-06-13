1b4a67db39456cb20a7c2b854fd16f592c2ec721
Chuck <chuck@eclecticsaddlebag.com>
2023-06-13 07:51:13 -0400

    Updates

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
-gem "rails", "~> 7.0.4"
+gem "rails", "~> 7.0.5"
 gem "sprockets-rails"
 gem "pg", "~> 1.1"
 gem "puma", "~> 6.3.0"
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require "rspec/rails"
 require "capybara/rails"
 require "fuubar"
-require 'webmock/rspec'
+require "webmock/rspec"
 
 Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
 
