57bd5a5d2c198849daa15656b9735437613cdc80
Chuck <chuck@eclecticsaddlebag.com>
2023-06-25 05:54:53 -0400

    Add test

--- spec/components/shared/footer_component_spec.rb
+++ spec/components/shared/footer_component_spec.rb
@@ -3,13 +3,8 @@
 require "rails_helper"
 
 RSpec.describe Shared::FooterComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
-
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "renders the footer" do
+    render_inline(described_class.new)
+    expect(rendered_content).to have_selector("footer")
+  end
 end
